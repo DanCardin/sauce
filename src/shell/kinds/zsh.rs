e6397881a8229e84cab3c16f6636cbddf3c944e0
Dan Cardin <ddcardin@gmail.com>
2021-01-24 09:06:57 -0500

    Add tests to shell implementations.

--- src/shell/kinds/zsh.rs
+++ src/shell/kinds/zsh.rs
@@ -52,3 +52,114 @@ impl Shell for Zsh {
         format!("unset -f {}", var)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    mod edit {
+        use super::super::*;
+        use pretty_assertions::assert_eq;
+
+        #[test]
+        fn it_edits_path() {
+            let shell = Zsh {};
+            let output = shell.edit("foo/bar");
+            assert_eq!(output, r#""$EDITOR" 'foo/bar'"#);
+        }
+    }
+
+    mod init {
+        use super::super::*;
+        use pretty_assertions::assert_eq;
+
+        #[test]
+        fn it_defaults() {
+            let shell = Zsh {};
+            let output = shell.init("foo", false);
+            assert_eq!(output, r#"function foo { eval "$(command foo "$@")" }"#);
+        }
+
+        #[test]
+        fn it_includes_autoload() {
+            let shell = Zsh {};
+            let output = shell.init("foo", true);
+            assert_eq!(
+                output,
+                r#"function foo { eval "$(command foo "$@")" }
+function _foo_autoload { foo --autoload }
+add-zsh-hook chpwd _foo_autoload"#
+            );
+        }
+    }
+
+    mod set_var {
+        use super::super::*;
+        use pretty_assertions::assert_eq;
+
+        #[test]
+        fn it_works() {
+            let shell = Zsh {};
+            let output = shell.set_var("foo", "bar");
+            assert_eq!(output, "export foo=bar");
+        }
+    }
+
+    mod set_alias {
+        use super::super::*;
+        use pretty_assertions::assert_eq;
+
+        #[test]
+        fn it_works() {
+            let shell = Zsh {};
+            let output = shell.set_alias("foo", "bar");
+            assert_eq!(output, "alias foo=bar");
+        }
+    }
+
+    mod set_function {
+        use super::super::*;
+        use pretty_assertions::assert_eq;
+
+        #[test]
+        fn it_works() {
+            let shell = Zsh {};
+            let output = shell.set_function("foo", "bar");
+            assert_eq!(output, "function foo {\n  bar\n}");
+        }
+    }
+
+    mod unset_var {
+        use super::super::*;
+        use pretty_assertions::assert_eq;
+
+        #[test]
+        fn it_works() {
+            let shell = Zsh {};
+            let output = shell.unset_var("foo");
+            assert_eq!(output, "unset foo");
+        }
+    }
+
+    mod unset_alias {
+        use super::super::*;
+        use pretty_assertions::assert_eq;
+
+        #[test]
+        fn it_works() {
+            let shell = Zsh {};
+            let output = shell.unset_alias("foo");
+            assert_eq!(output, "unalias foo 2>/dev/null");
+        }
+    }
+
+    mod unset_function {
+        use super::super::*;
+        use pretty_assertions::assert_eq;
+
+        #[test]
+        fn it_works() {
+            let shell = Zsh {};
+            let output = shell.unset_function("foo");
+            assert_eq!(output, "unset -f foo");
+        }
+    }
+}
